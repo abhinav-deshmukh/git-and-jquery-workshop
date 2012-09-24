61682baacd1187b0de9140dad5ff4e3d19c38c97
Abhinav Deshmukh <abhinavad@gmail.com>
2012-09-24 12:15:23 +0800

    commit2

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@
 // $("img").width(200);
 
 $(document).ready(function(){
+	$("li").on("click",function(){
+		if ($(this).hasClass("done")){
+			$(this).removeClass("done");
+		}
+		else
+			$(this).addClass("done");
+      //$("li").addClass("done");
+	});
 	$("img").on("dblclick", function(){
 	$("img").width(200);
 	$("#rahul").hide();
 	$(".booboo").slideUp();
-		
+
 	});
 	// $("img").click(function(){
 	// 	$("img").width(100);
