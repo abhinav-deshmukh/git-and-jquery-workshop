aaf80e0beb3905f467862ae8da047830f1850cd0
Abhinav Deshmukh <abhinavad@gmail.com>
2012-09-24 11:49:34 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// console.log("main.js is loded");
+// $("img").width(200);
+
+$(document).ready(function(){
+	$("img").on("dblclick", function(){
+	$("img").width(200);
+	$("#rahul").hide();
+	$(".booboo").slideUp();
+		
+	});
+	// $("img").click(function(){
+	// 	$("img").width(100);
+	// });
+});
